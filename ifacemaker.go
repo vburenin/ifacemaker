9572533fa410ce541da728309c186e640f20fe37
xtakei <xtakei@me.com>
2018-07-31 12:27:25 +0900

    * support embedded struct * support excluding method

--- ifacemaker.go
+++ ifacemaker.go
@@ -5,18 +5,19 @@ import (
 	"io/ioutil"
 	"log"
 
+	"github.com/kazu/ifacemaker/maker"
 	"github.com/mkideal/cli"
-	"github.com/vburenin/ifacemaker/maker"
 )
 
 type cmdlineArgs struct {
 	cli.Helper
-	Files      []string `cli:"*f,file" usage:"Go source file to read"`
-	StructType string   `cli:"*s,struct" usage:"Generate an interface for this structure name"`
-	IfaceName  string   `cli:"*i,iface" usage:"Name of the generated interface"`
-	PkgName    string   `cli:"*p,pkg" usage:"Package name for the generated interface"`
-	CopyDocs   bool     `cli:"d,doc" usage:"Copy docs from methods" dft:"true"`
-	Output     string   `cli:"o,output" usage:"Output file name. If not provided, result will be printed to stdout."`
+	Files          []string `cli:"*f,file" usage:"Go source file to read"`
+	StructType     string   `cli:"*s,struct" usage:"Generate an interface for this structure name"`
+	IfaceName      string   `cli:"*i,iface" usage:"Name of the generated interface"`
+	PkgName        string   `cli:"*p,pkg" usage:"Package name for the generated interface"`
+	ExcludeMethods []string `cli:"*e,exclude" usage:"Method name for not generation " `
+	CopyDocs       bool     `cli:"d,doc" usage:"Copy docs from methods" dft:"true"`
+	Output         string   `cli:"o,output" usage:"Output file name. If not provided, result will be printed to stdout."`
 }
 
 func run(args *cmdlineArgs) {
@@ -24,23 +25,45 @@ func run(args *cmdlineArgs) {
 	allImports := []string{}
 	mset := make(map[string]struct{})
 	iset := make(map[string]struct{})
+	parseds := make(map[string]*maker.Parsed)
+	fmt.Printf("files=%s\n", args.Files)
 	for _, f := range args.Files {
 		src, err := ioutil.ReadFile(f)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		methods, imports := maker.ParseStruct(src, args.StructType, args.CopyDocs)
-		for _, m := range methods {
-			if _, ok := mset[m.Code]; !ok {
-				allMethods = append(allMethods, m.Lines()...)
-				mset[m.Code] = struct{}{}
+		//parseds := maker.ParseStruct(src, args.StructType, args.CopyDocs)
+		sparseds := maker.ParseStruct(src, args.CopyDocs, args.ExcludeMethods)
+		for s, parsed := range sparseds {
+			parseds[s] = parsed
+		}
+	}
+	var methods []maker.Method
+	var imports []string
+
+	if parseds[args.StructType] != nil {
+		methods = append(methods, parseds[args.StructType].Methods...)
+		imports = parseds[args.StructType].Imports
+
+		if len(parseds[args.StructType].Embedded) > 0 {
+			for _, embed := range parseds[args.StructType].Embedded {
+				if parseds[embed] != nil {
+					methods = append(methods, parseds[embed].Methods...)
+				}
 			}
 		}
-		for _, i := range imports {
-			if _, ok := iset[i]; !ok {
-				allImports = append(allImports, i)
-				iset[i] = struct{}{}
-			}
+	}
+
+	for _, m := range methods {
+		if _, ok := mset[m.Code]; !ok {
+			allMethods = append(allMethods, m.Lines()...)
+			mset[m.Code] = struct{}{}
+		}
+	}
+	for _, i := range imports {
+		if _, ok := iset[i]; !ok {
+			allImports = append(allImports, i)
+			iset[i] = struct{}{}
 		}
 	}
 
--- maker/maker.go
+++ maker/maker.go
@@ -16,6 +16,12 @@ type Method struct {
 	Docs []string
 }
 
+type Parsed struct {
+	Embedded []string
+	Methods  []Method
+	Imports  []string
+}
+
 func (m *Method) Lines() []string {
 	var lines []string
 	lines = append(lines, m.Docs...)
@@ -37,7 +43,35 @@ func GetReceiverTypeName(src []byte, fl interface{}) (string, *ast.FuncDecl) {
 		st = st[1:]
 	}
 	return st, fd
+}
+func GetStructName(src []byte, dec interface{}) string {
 
+	if gd, ok := dec.(*ast.GenDecl); ok {
+		if gd.Tok == token.TYPE {
+			if ts, ok := gd.Specs[0].(*ast.TypeSpec); ok {
+				return ts.Name.Name
+			}
+		}
+	}
+	return ""
+}
+
+func GetEmbedded(src []byte, dec interface{}) (embeds []string) {
+
+	if gd, ok := dec.(*ast.GenDecl); ok {
+		if gd.Tok == token.TYPE {
+			if ts, ok := gd.Specs[0].(*ast.TypeSpec); ok {
+				if st, ok := ts.Type.(*ast.StructType); ok {
+					for _, l := range st.Fields.List {
+						if l.Names == nil {
+							embeds = append(embeds, l.Type.(*ast.Ident).Name)
+						}
+					}
+				}
+			}
+		}
+	}
+	return embeds
 }
 
 func GetParameters(src []byte, fl *ast.FieldList) ([]string, bool) {
@@ -98,7 +132,24 @@ func MakeInterface(pkgName, ifaceName string, methods []string, imports []string
 	return FormatCode(code)
 }
 
-func ParseStruct(src []byte, structName string, copyDocs bool) (methods []Method, imports []string) {
+type StringSlice []string
+
+func (strs StringSlice) Contain(s string) bool {
+
+	for _, src := range strs {
+		if src == s {
+			return true
+		}
+	}
+
+	return false
+}
+
+func ParseStruct(src []byte, copyDocs bool, exs []string) (parseds map[string]*Parsed) {
+
+	parseds = make(map[string]*Parsed)
+	var imports []string
+
 	fset := token.NewFileSet()
 	a, err := parser.ParseFile(fset, "", src, parser.ParseComments)
 	if err != nil {
@@ -114,32 +165,58 @@ func ParseStruct(src []byte, structName string, copyDocs bool) (methods []Method
 	}
 
 	for _, d := range a.Decls {
-		if a, fd := GetReceiverTypeName(src, d); a == structName {
-			methodName := fd.Name.String()
+
+		if name := GetStructName(src, d); name != "" {
+			if parseds[name] == nil {
+				parseds[name] = &Parsed{}
+			}
+			parseds[name].Embedded = append(parseds[name].Embedded, GetEmbedded(src, d)...)
+			continue
+		}
+
+		a, fd := GetReceiverTypeName(src, d)
+		var methodName string
+		if fd != nil {
+
+			methodName = fd.Name.String()
 			if methodName[0] > 'Z' {
 				continue
 			}
-			params, _ := GetParameters(src, fd.Type.Params)
-			ret, merged := GetParameters(src, fd.Type.Results)
+			if StringSlice(exs).Contain(methodName) {
+				continue
+			}
 
-			var retValues string
-			if merged {
-				retValues = fmt.Sprintf("(%s)", strings.Join(ret, ", "))
-			} else {
-				retValues = strings.Join(ret, ", ")
+			if parseds[a] == nil {
+				parseds[a] = &Parsed{}
 			}
-			method := fmt.Sprintf("%s(%s) %s", methodName, strings.Join(params, ", "), retValues)
-			var docs []string
-			if fd.Doc != nil && copyDocs {
-				for _, d := range fd.Doc.List {
-					docs = append(docs, string(src[d.Pos()-1:d.End()-1]))
-				}
-			}
-			methods = append(methods, Method{
-				Code: method,
-				Docs: docs,
-			})
+		} else {
+
+			continue
 		}
+
+		params, _ := GetParameters(src, fd.Type.Params)
+		ret, merged := GetParameters(src, fd.Type.Results)
+
+		var retValues string
+		if merged {
+			retValues = fmt.Sprintf("(%s)", strings.Join(ret, ", "))
+		} else {
+			retValues = strings.Join(ret, ", ")
+		}
+		method := fmt.Sprintf("%s(%s) %s", methodName, strings.Join(params, ", "), retValues)
+		var docs []string
+		if fd.Doc != nil && copyDocs {
+			for _, d := range fd.Doc.List {
+				docs = append(docs, string(src[d.Pos()-1:d.End()-1]))
+			}
+		}
+
+		parseds[a].Methods = append(parseds[a].Methods, Method{
+			Code: method,
+			Docs: docs,
+		})
+		parseds[a].Imports = imports
 	}
+
 	return
 }
