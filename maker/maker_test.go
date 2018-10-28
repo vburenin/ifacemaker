b2d65632ed52c8b8911a4a82db05e9e323ba8f4a
Matti Andreas Nielsen <matti@denlillemand.com>
2018-10-28 22:08:28 +0100

    Comments, Tests and a little refactoring
    Fixed #22 which was kind of a bug converting
    all named return values to unnamed return
    values in the interface.
    
    GetParameters was used for both return values
    and parameters which was a little confusing
    i thouth, so i changed the name of it.
    The parameters use of it also threw away
    the merged boolean, which i didnt like
    too much, so decided to get rid of the
    merged boolean, because it was only
    used to wrap the return values in
    paranthesis or not. Now we just always
    wrap return values in paranthesis
    because the FormatCode just removes
    them anyway.
    
    Impl a new method GetReceiverType
    which checks if the receiver is nil
    to decide whether the FuncDecl
    is a method or a function. The
    other impl by calling GetFields
    was also ok, because it was guarded
    against getting called on a nil
    FuncDecl.Recv object, but i think
    it is cleaner just to check it
    yourself.
    
    New implementation of the private
    function check to make the logic
    of the check explicit.
    
    Also adding some unit tests for some
    of the old and new methods.

--- maker/maker_test.go
+++ maker/maker_test.go
@@ -1,8 +1,73 @@
 package maker
 
 import (
+	"go/parser"
+	"go/token"
+	"log"
 	"strings"
 	"testing"
+)
+
+var (
+	src = []byte(`
+		package main
+
+		import (
+		    "fmt"
+		)
+
+		// Person ...
+		type Person struct {
+		    name string
+		    age int
+		    telephone string
+		}
+
+		// Name ...
+		func (p *Person) Name() string {
+		    return p.name
+		}
+
+		// SetName ...
+		func (p *Person) SetName(name string) {
+		    p.name = name
+		}
+
+		// Age ...
+		func (p *Person) Age() int {
+		    return p.Age
+		}
+
+		// Age ...
+		func (p *Person) SetAge(age int) int {
+		    p.Age = age
+		}
+
+		// AgeAndName ...
+		func (p *Person) AgeAndName() (int, string) {
+		    return p.age, p.name
+		}
+
+		func (p *Person) SetAgeAndName(name string, age int) {
+		    p.name = name
+		    p.age = age
+		}
+
+		// TelephoneAndName ...
+		func (p *Person) GetNameAndTelephone() (name, telephone string) {
+		    telephone = p.telephone
+		    name = p.name 
+		    return
+		}
+
+		func (p *Person) SetNameAndTelephone(name, telephone string) {
+		    p.name = name
+		    p.telephone = telephone
+		}
+
+		func SomeFunction() string {
+		    return "Something"
+		}`)
 )
 
 func check(value, pattern string, t *testing.T) {
@@ -21,25 +86,113 @@ func TestLines(t *testing.T) {
 }
 
 func TestParseStruct(t *testing.T) {
-	src := []byte(`package main
-	    
-	    import (
-		"fmt"
-	    )
-
-	    // Person ...
-	    type Person struct {
-		name string
-	    }
-
-	    // Name ...
-	    func (p *Person) Name() string {
-		return p.name
-	    }`)
 	methods, imports := ParseStruct(src, "Person", true)
-	check(methods[0].Code, "Name() string", t)
+	check(methods[0].Code, "Name() (string)", t)
 	imp := imports[0]
 	trimmedImp := strings.TrimSpace(imp)
 	expected := "\"fmt\""
 	check(trimmedImp, expected, t)
+}
+
+func TestGetReceiverTypeName(t *testing.T) {
+	fset := token.NewFileSet()
+	a, err := parser.ParseFile(fset, "", src, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	hasPersonFuncDecl := false
+	for _, d := range a.Decls {
+		typeName, fd := GetReceiverTypeName(src, d)
+		if typeName == "" {
+			continue
+		}
+		switch typeName {
+		case "Person":
+			if fd == nil {
+				t.Fatalf("receiver type with name %s had a nil func decl", typeName)
+			}
+			// OK
+			hasPersonFuncDecl = true
+		}
+	}
+	if !hasPersonFuncDecl {
+		t.Fatalf("Never registered a func decl with the `Person` receiver type")
+	}
+}
+
+func TestIsFunctionPrivate(t *testing.T) {
+	functionNames := map[string]bool{"_someFunc": true, "herp": true, "_SomeFunc": true, "SomeFunc": false, "ZooFunc": false}
+	for name, expected := range functionNames {
+		if isFunctionPrivate(name) != expected {
+			t.Fatalf("Function name %s != expected value %+v", name, expected)
+		}
+	}
+
+}
+
+func TestFormatFieldList(t *testing.T) {
+	fset := token.NewFileSet()
+	a, err := parser.ParseFile(fset, "", src, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	for _, d := range a.Decls {
+		if a, fd := GetReceiverTypeName(src, d); a == "Person" {
+			methodName := fd.Name.String()
+			params := FormatFieldList(src, fd.Type.Params)
+			results := FormatFieldList(src, fd.Type.Results)
+			switch methodName {
+			case "Name":
+				expectedParam := []string{}
+				expectedResults := []string{"string"}
+				if !compare(expectedParam, params, t) || !compare(expectedResults, results, t) {
+					t.Fatalf("Name did not have the expected params and/or results")
+				}
+			case "Age":
+				expectedParams := []string{}
+				expectedResults := []string{"int"}
+				if !compare(expectedParams, params, t) || !compare(expectedResults, results, t) {
+					t.Fatalf("Age did not have the expected params and/or results")
+				}
+			case "SetName":
+				expectedParams := []string{"name string"}
+				expectedResults := []string{}
+				if !compare(expectedParams, params, t) || !compare(expectedResults, results, t) {
+					t.Fatalf("SetName did not have the expected params and/or results")
+				}
+			case "SetAgeAndName":
+				expectedParams := []string{"name string", "age int"}
+				expectedResults := []string{}
+				if !compare(expectedParams, params, t) || !compare(expectedResults, results, t) {
+					t.Fatalf("SetAgeAndName did not have the expected params and/or results")
+				}
+			case "GetNameAndTelephone":
+				expectedParams := []string{}
+				expectedResults := []string{"name, telephone string"}
+				if !compare(expectedParams, params, t) || !compare(expectedResults, results, t) {
+					t.Fatalf("GetNameAndTelephone did not have the expected params and/or results")
+				}
+			case "SetNameAndTelephone":
+				expectedParams := []string{"name, telephone string"}
+				expectedResults := []string{}
+				if !compare(expectedParams, params, t) || !compare(expectedResults, results, t) {
+					t.Fatalf("SetNameAndTelephone did not have the expected params and/or results")
+				}
+			}
+		}
+	}
+}
+
+func compare(actual []string, expected []string, t *testing.T) bool {
+	if len(actual) != len(expected) {
+		t.Logf("Compare received two different lengths of fields, expected:|%+v| was not equal to actual |%+v|. actual length:%d, expected length:%d", expected, actual, len(actual), len(expected))
+		return false
+	}
+	for i := 0; i < len(actual); i++ {
+		if actual[i] != expected[i] {
+			t.Logf("Compare expected:|%+v| was not equal to actual |%+v|", expected, actual)
+			return false
+		}
+	}
+	return true
 }
